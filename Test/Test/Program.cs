f68f22da90c24e1598f3cbb328ef2afee3743190
Александр Жаворонок <C0baltt@live.com>
2021-04-07 11:02:11 +0300

    в тестовой программе разобрался с датой

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -4,18 +4,21 @@
 
 namespace Test
 {
-   
+    /*
+     (*) если введенный день сегодня - вывести "{xx} - это сегодня"
+     (**) вывести дату ближайшего дня недели который совпадает с введенным. 
+    Если день сегодняшний, то дата нужна через неделю.
+    */
     class Program
     {
         static void Main(string[] args)
         {
-            Operation op;
-            op = Operation.Subtract;
-            Console.WriteLine($"{op} - {(int)op}"); // Add
 
-           
-            //op = Operation.Multiply;
-           // Console.WriteLine((int)op);
+            Console.WriteLine($"DateTime.Now -- {DateTime.Now}");//07.04.2021 10:54:30
+            Console.WriteLine($"DateTime.UtcNow -- {DateTime.UtcNow}");//07.04.2021 7:54:30
+            Console.WriteLine($"DateTime.Today -- {DateTime.Today}");//07.04.2021 0:00:00
+            
+            Console.WriteLine($"DateTime.Today.DayOfWeek -- {DateTime.Today.DayOfWeek}");
 
             Console.ReadLine();
         }
@@ -27,4 +30,13 @@ namespace Test
         Multiply,
         Divide
     }
+
 }
+
+/* Operation op;
+ op = Operation.Subtract;
+ Console.WriteLine($"{op} - {(int)op}"); // Add
+
+
+ //op = Operation.Multiply;
+// Console.WriteLine((int)op);*/
