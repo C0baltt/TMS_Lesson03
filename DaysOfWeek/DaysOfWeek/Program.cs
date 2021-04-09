21657536104891bc1b20538f08fb1a3129ceae98
Александр Жаворонок <C0baltt@live.com>
2021-04-09 22:41:06 +0300

    программа проверки дней недели готова

--- DaysOfWeek/DaysOfWeek/Program.cs
+++ DaysOfWeek/DaysOfWeek/Program.cs
@@ -8,49 +8,13 @@ namespace DaysOfWeek
         {
             for (int i = 1; i < 9; i++)
             {
-                /*//====================для отладки, в финальной версии удалить=============================================
-                //Для автоматической проверки раскоментируй здесь, закомментируй стоку 46
-                string temp = null;
-                switch (i)
-                {
-                    case 1:
-                        temp = "Mon";
-                        break;
-                    case 2:
-                        temp = "Tue";
-                        break;
-                    case 3:
-                        temp = "Wed";
-                        break;
-                    case 4:
-                        temp = "Thu";
-                        break;
-                    case 5:
-                        temp = "Fri";
-                        break;
-                    case 6:
-                        temp = "Sat";
-                        break;
-                    case 7:
-                        temp = "Sun";
-                        break;
-                    case 8:
-                        temp = "Error";
-                        break;
-                    default:
-                        break;
-                }//*/
-                //===================================================================
-
-                //Для ручной проверки раскоментируй здесь, закомментируй стоку 19
-                Console.WriteLine($"{i}\nВведи день недели. Для выхода введи \"quit\" или \"exit\" или \"q\"");
+                Console.WriteLine($"{i} из 8 итераций\nВведи день недели. Для выхода введи \"quit\" или \"exit\" или \"q\"");
                 string temp = Console.ReadLine();//*/
                 temp = temp.Trim().ToLower();
 
                 DayOfWeek day = DayOfWeek.Monday;
 
                 bool thisDayDoesNotExist = false;
-
                 switch (temp)
                 {
                     case "mon" or "monday":
@@ -90,27 +54,23 @@ namespace DaysOfWeek
                         Console.Write("Такого дня недели не существует\n");
                         thisDayDoesNotExist = true;
                         //Console.ResetColor();
-
                         break;
                 }               
                 
                 if (thisDayDoesNotExist == false)
                 {
                     Console.Write($"{day}");
+
                     var daysBeforeTheWeekend = DayOfWeek.Saturday - day;
-                    
                     if ((daysBeforeTheWeekend < 0) || (daysBeforeTheWeekend > 5))
                     { 
                         daysBeforeTheWeekend = 0; 
                     }
                     Console.WriteLine($". Порядковый номер дня недели {(int)day}. До выходных осталось {daysBeforeTheWeekend} дней");
-
                     Console.ResetColor(); // сбрасываем в стандартный
-
                     var numderOfDaysOfTheWeek = 7;//чтобы не было магических чисел, создаю переменную
                     if (DateTime.Today.DayOfWeek == day)
                     {
-                        
                         Console.WriteLine($"Сегодня как раз {DateTime.Today.DayOfWeek}, следующий будет {DateTime.Today.AddDays(numderOfDaysOfTheWeek).ToShortDateString()}");
                         DateTime.Today.AddDays((int)DayOfWeek.Sunday);
                     }
@@ -132,8 +92,3 @@ namespace DaysOfWeek
 
     }
 }
-/*
-     (*) если введенный день сегодня - вывести "{xx} - это сегодня"
-     (**) вывести дату ближайшего дня недели который совпадает с введенным. 
-    Если день сегодняшний, то дата нужна через неделю.
-    */
